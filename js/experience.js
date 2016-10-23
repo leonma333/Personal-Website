c91fec4a98e073dbf3cedbb9bc7b6eaac7cf4c0b
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-10-23 11:51:13 -0400

    progress in experience page

--- js/experience.js
+++ js/experience.js
@@ -1,47 +1,41 @@
 function initHistoryHandler() {
-    var clock = $('#history-clock');
-
-    $('.about_nav_content li:first-child a').click( function() {
-       	$('#historic-event-1').addClass('current');
-       	$('#historic-event-2').removeClass('current');
-       	$('#historic-event-3').removeClass('current');
-       	$('#historic-event-4').removeClass('current');
-       	clock.removeClass('animate1 animate2 animate3');
-    });
+    var clock = $("#history-clock");
+    var yearList = $(".historic-year p");
        	
     var hoverOrClick = function() {
-        clock.removeClass('animate1 animate2 animate3');
-       	$('#historic-event-1').addClass('current');
-       	$('#historic-event-2, #historic-event-3, #historic-event-4').removeClass('current');
+        clock.removeClass("animate1 animate2 animate3");
+       	$(this).addClass("current");
+        yearList.eq(0).css("font-size", "18px");
+       	$("#historic-event-2, #historic-event-3, #historic-event-4").removeClass("current");
     }
 
-    $('#historic-dot-1, #historic-event-1').click(hoverOrClick).hover(hoverOrClick);
+    $("#historic-dot-1, #historic-event-1").click(hoverOrClick).hover(hoverOrClick);
        	
-    var hoverOrClick = function(){
-        clock.addClass('animate1');
-        clock.removeClass('animate2 animate3');
-        $('#historic-event-2').addClass('current');
-        $('#historic-event-1, #historic-event-3, #historic-event-4').removeClass('current');
+    hoverOrClick = function(){
+        clock.addClass("animate1");
+        clock.removeClass("animate2 animate3");
+        $("#historic-event-2").addClass("current");
+        $("#historic-event-1, #historic-event-3, #historic-event-4").removeClass("current");
     }
 
-    $('#historic-dot-2, #historic-event-2').click(hoverOrClick).hover(hoverOrClick);
+    $("#historic-dot-2, #historic-event-2").click(hoverOrClick).hover(hoverOrClick);
        	
-    var hoverOrClick = function(){
-        clock.addClass('animate2');
-       	clock.removeClass('animate1 animate3');
-       	$('#historic-event-3').addClass('current');
-       	$('#historic-event-1, #historic-event-2, #historic-event-4').removeClass('current');
+    hoverOrClick = function(){
+        clock.addClass("animate2");
+       	clock.removeClass("animate1 animate3");
+       	$("#historic-event-3").addClass("current");
+       	$("#historic-event-1, #historic-event-2, #historic-event-4").removeClass("current");
     }
 
-    $('#historic-dot-3, #historic-event-3').click(hoverOrClick).hover(hoverOrClick);
+    $("#historic-dot-3, #historic-event-3").click(hoverOrClick).hover(hoverOrClick);
        	
-    var hoverOrClick = function(){
-       	clock.addClass('animate3');
-       	clock.removeClass('animate1 animate2');
-       	$('#historic-event-4').addClass('current');
-       	$('#historic-event-1, #historic-event-2, #historic-event-3').removeClass('current');
+    hoverOrClick = function(){
+       	clock.addClass("animate3");
+       	clock.removeClass("animate1 animate2");
+       	$("#historic-event-4").addClass("current");
+       	$("#historic-event-1, #historic-event-2, #historic-event-3").removeClass("current");
     }
 
-    $('#historic-dot-4, #historic-event-4').click(hoverOrClick).hover(hoverOrClick);
+    $("#historic-dot-4, #historic-event-4").click(hoverOrClick).hover(hoverOrClick);
 }
     
