c643b38267cf9278c157fb1076899069e820af4c
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-07-29 21:00:18 -0400

    add kicks4love to experience

--- js/experience.js
+++ js/experience.js
@@ -3,61 +3,88 @@ function initHistoryHandler() {
     var yearList = $(".historic-year p");
        	
     var hoverOrClick = function() {
-        clock.removeClass("animate1 animate2 animate3");
+        clock.removeClass("animate1 animate2 animate3 animate4");
        	$(this).addClass("current");
         yearList.eq(0).css("font-size", "18px");
         for (var i = 1; i < yearList.length; i++) 
           yearList.eq(i).css("font-size", "16px");
-       	$("#historic-event-2, #historic-event-3, #historic-event-4").removeClass("current");
+       	$("#historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5").removeClass("current");
     }
 
     $("#historic-dot-1, #historic-event-1").on("click mouseover touchstart", hoverOrClick);
        	
     hoverOrClick = function() {
         clock.addClass("animate1");
-        clock.removeClass("animate2 animate3");
+        clock.removeClass("animate2 animate3 animate4");
         $(this).addClass("current");
         yearList.eq(1).css("font-size", "18px");
         for (var i = 0; i < yearList.length; i++) { 
           if (i == 1) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-        $("#historic-event-1, #historic-event-3, #historic-event-4").removeClass("current");
+        $("#historic-event-1, #historic-event-3, #historic-event-4, #historic-event-5").removeClass("current");
     }
 
     $("#historic-dot-2, #historic-event-2").on("click mouseover touchstart", hoverOrClick);
        	
     hoverOrClick = function() {
         clock.addClass("animate2");
-       	clock.removeClass("animate1 animate3");
+       	clock.removeClass("animate1 animate3 animate4");
        	$(this).addClass("current");
         yearList.eq(2).css("font-size", "18px");
         for (var i = 2; i < yearList.length; i++) { 
           if (i == 2) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-       	$("#historic-event-1, #historic-event-2, #historic-event-4").removeClass("current");
+       	$("#historic-event-1, #historic-event-2, #historic-event-4, #historic-event-5").removeClass("current");
     }
 
     $("#historic-dot-3, #historic-event-3").on("click mouseover touchstart", hoverOrClick);
        	
     hoverOrClick = function() {
        	clock.addClass("animate3");
-       	clock.removeClass("animate1 animate2");
+       	clock.removeClass("animate1 animate2 animate4");
        	$(this).addClass("current");
         yearList.eq(3).css("font-size", "18px");
-        for (var i = 0; i < yearList.length - 1; i++)
+        for (var i = 0; i < yearList.length; i++) {
+          if (i == 3) continue;
           yearList.eq(i).css("font-size", "16px");
-       	$("#historic-event-1, #historic-event-2, #historic-event-3").removeClass("current");
+        }
+       	$("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-5").removeClass("current");
     }
 
     $("#historic-dot-4, #historic-event-4").on("click mouseover touchstart", hoverOrClick);
 
+    hoverOrClick = function() {
+        clock.addClass("animate4");
+        clock.removeClass("animate1 animate2 animate3");
+        $(this).addClass("current");
+        yearList.eq(4).css("font-size", "18px");
+        for (var i = 0; i < yearList.length - 1; i++)
+          yearList.eq(i).css("font-size", "16px");
+        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4").removeClass("current");
+    }
+
+    $("#historic-dot-5, #historic-event-5").on("click mouseover touchstart", hoverOrClick);
+
     // set up tooltip
     var titleList = jQuery(".back h3");
 
-    // Jewlr.com
+    // Kicks4Love
     titleList.eq(0).tipso({
+        background: "white",
+        titleBackground: "#c3c3c3",
+        color: "black",
+        tooltipHover: true,
+        titleContent: "Kicks4Love",       
+        position: "bottom-right",
+        size: "default",
+        content: "<p>40% Ruby (RoR); 40% Javascript (React Native); 20% Bash;</p><br><p>Ruby on Rails, React Native, Ngnix, Ngrok</p><br>" +
+                 "<a style='color:black' target='_blank' href='https://kicks4love.com'>See Website</a>"
+    });
+
+    // Jewlr.com
+    titleList.eq(1).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
@@ -70,7 +97,7 @@ function initHistoryHandler() {
     });
 
     // Ashlin BPG Marketing
-    titleList.eq(1).tipso({
+    titleList.eq(2).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
@@ -83,7 +110,7 @@ function initHistoryHandler() {
     });
 
     // University of Waterloo
-    titleList.eq(2).tipso({
+    titleList.eq(3).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
@@ -91,12 +118,12 @@ function initHistoryHandler() {
         titleContent: "University of Waterloo",       
         position: "bottom-left",
         size: "default",
-        content: "<p>Current: 2A</p><br><p>1.Algorithms and Data Structures</p><p>2.Embedded Systems</p><p>3.Digital Computers</p>" +
-                 "<p>4.Advanced Calculus</p><br><a style='color:black' target='_blank' href='https://uwaterloo.ca'>See Website</a>"
+        content: "<p>Current: 2B</p><br><p>1.Algorithms and Data Structures</p><p>2.Operating Systems</p><p>3.Embedded Microprocessor Systems</p>" +
+                 "<p>4.Digital Computers</p><br><a style='color:black' target='_blank' href='https://uwaterloo.ca'>See Website</a>"
     });
 
     // Shanghai United International School
-    titleList.eq(3).tipso({
+    titleList.eq(4).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
