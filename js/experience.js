42f387ac978d201db863adeeb5588dfef8efb410
Liang-Hsuan Ma <leonma333@hotmail.com>
2019-08-04 17:17:39 -0400

    Update experience

--- js/experience.js
+++ js/experience.js
@@ -3,109 +3,134 @@ function initHistoryHandler() {
     var yearList = $(".historic-year p");
 
     var hoverOrClick = function() {
-        clock.removeClass("animate1 animate2 animate3 animate4 animate5 animate6");
+        clock.removeClass("animate1 animate2 animate3 animate4 animate5 animate6 animate7");
         $(this).addClass("current");
         yearList.eq(0).css("font-size", "18px");
         for (var i = 1; i < yearList.length; i++)
           yearList.eq(i).css("font-size", "16px");
-        $("#historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7").removeClass("current");
+        $("#historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-1, #historic-event-1").on("click mouseover touchstart", hoverOrClick);
 
     hoverOrClick = function() {
         clock.addClass("animate1");
-        clock.removeClass("animate2 animate3 animate4 animate5 animate6");
+        clock.removeClass("animate2 animate3 animate4 animate5 animate6 animate7");
         $(this).addClass("current");
         yearList.eq(1).css("font-size", "18px");
         for (var i = 0; i < yearList.length; i++) {
           if (i == 1) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-        $("#historic-event-1, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7").removeClass("current");
+        $("#historic-event-1, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-2, #historic-event-2").on("click mouseover touchstart", hoverOrClick);
 
     hoverOrClick = function() {
         clock.addClass("animate2");
-        clock.removeClass("animate1 animate3 animate4 animate5 animate6");
+        clock.removeClass("animate1 animate3 animate4 animate5 animate6 animate7");
         $(this).addClass("current");
         yearList.eq(2).css("font-size", "18px");
         for (var i = 2; i < yearList.length; i++) {
           if (i == 2) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-        $("#historic-event-1, #historic-event-2, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7").removeClass("current");
+        $("#historic-event-1, #historic-event-2, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-3, #historic-event-3").on("click mouseover touchstart", hoverOrClick);
 
     hoverOrClick = function() {
         clock.addClass("animate3");
-        clock.removeClass("animate1 animate2 animate4 animate5 animate6");
+        clock.removeClass("animate1 animate2 animate4 animate5 animate6 animate7");
         $(this).addClass("current");
         yearList.eq(3).css("font-size", "18px");
         for (var i = 0; i < yearList.length; i++) {
           if (i == 3) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-5, #historic-event-6, #historic-event-7").removeClass("current");
+        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-5, #historic-event-6, #historic-event-7, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-4, #historic-event-4").on("click mouseover touchstart", hoverOrClick);
 
     hoverOrClick = function() {
         clock.addClass("animate4");
-        clock.removeClass("animate1 animate2 animate3 animate5 animate6");
+        clock.removeClass("animate1 animate2 animate3 animate5 animate6 animate7");
         $(this).addClass("current");
         yearList.eq(4).css("font-size", "18px");
         for (var i = 0; i < yearList.length; i++) {
           if (i == 4) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-6, #historic-event-7").removeClass("current");
+        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-6, #historic-event-7, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-5, #historic-event-5").on("click mouseover touchstart", hoverOrClick);
 
     hoverOrClick = function() {
         clock.addClass("animate5");
-        clock.removeClass("animate1 animate2 animate3 animate4 animate6");
+        clock.removeClass("animate1 animate2 animate3 animate4 animate6 animate7");
         $(this).addClass("current");
         yearList.eq(5).css("font-size", "18px");
         for (var i = 0; i < yearList.length; i++) {
           if (i == 5) continue;
           yearList.eq(i).css("font-size", "16px");
         }
-        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-dot-7").removeClass("current");
+        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-dot-7, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-6, #historic-event-6").on("click mouseover touchstart", hoverOrClick);
 
     hoverOrClick = function() {
         clock.addClass("animate6");
-        clock.removeClass("animate1 animate2 animate3 animate4 animate5");
+        clock.removeClass("animate1 animate2 animate3 animate4 animate5 animate7");
         $(this).addClass("current");
         yearList.eq(6).css("font-size", "18px");
         for (var i = 0; i < yearList.length - 1; i++)
           yearList.eq(i).css("font-size", "16px");
-        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6").removeClass("current");
+        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-8").removeClass("current");
     }
 
     $("#historic-dot-7, #historic-event-7").on("click mouseover touchstart", hoverOrClick);
 
+    hoverOrClick = function() {
+        clock.addClass("animate7");
+        clock.removeClass("animate1 animate2 animate3 animate4 animate5 animate6");
+        $(this).addClass("current");
+        yearList.eq(6).css("font-size", "18px");
+        for (var i = 0; i < yearList.length - 1; i++)
+          yearList.eq(i).css("font-size", "16px");
+        $("#historic-event-1, #historic-event-2, #historic-event-3, #historic-event-4, #historic-event-5, #historic-event-6, #historic-event-7").removeClass("current");
+    }
+
+    $("#historic-dot-8, #historic-event-8").on("click mouseover touchstart", hoverOrClick);
+
     // set up tooltip
     var titleList = jQuery(".back h3");
 
-    // Freshbooks
+    // Wish
     titleList.eq(0).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
         tooltipHover: true,
-        titleContent: "<strong>FreshBooks</strong>",
+        titleContent: "<strong>Wish</strong>",
         position: "bottom-right",
+        width: 300,
+        content: "<p>Coming soon</p><br>" +
+                "<a style='color:black' target='_blank' href='https://www.wish.com/'>See Website</a>"
+    });
+
+    // Freshbooks
+    titleList.eq(1).tipso({
+        background: "white",
+        titleBackground: "#c3c3c3",
+        color: "black",
+        tooltipHover: true,
+        titleContent: "<strong>FreshBooks</strong>",
+        position: "bottom-left",
         width: 300,
         content: "<ul>" +
                 "<li><b>$</b> Created time tracking feature on admin software.</li>" +
@@ -118,13 +143,13 @@ function initHistoryHandler() {
     });
 
     // Shopify
-    titleList.eq(1).tipso({
+    titleList.eq(2).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
         tooltipHover: true,
         titleContent: "<strong>Shopify</strong>",
-        position: "bottom-left",
+        position: "bottom-right",
         width: 300,
         content: "<ul>" +
                 "<li><b>$</b> Designed and developed a fault tolerance, low latency and high throughput Kafka stream processing application.</li>" +
@@ -137,13 +162,13 @@ function initHistoryHandler() {
     });
 
     // Kicks4Love
-    titleList.eq(2).tipso({
+    titleList.eq(3).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
         tooltipHover: true,
         titleContent: "<strong>Kicks4Love</strong>",
-        position: "bottom-right",
+        position: "bottom-left",
         width: 300,
         content: "<ul>" +
                 "<li><b>$</b> Started multi-language Kicks4Love web and app from scratch while arranging advertisement contract on the platform.</li>" +
@@ -156,13 +181,13 @@ function initHistoryHandler() {
     });
 
     // Jewlr.com
-    titleList.eq(3).tipso({
+    titleList.eq(4).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
         tooltipHover: true,
         titleContent: "<strong>Jewlr.com</strong>",
-        position: "bottom-left",
+        position: "bottom-right",
         width: 300,
         content: "<ul>" +
                 "<li><b>$</b> Added new features to public commercial website and internal management system.</li>" +
@@ -175,13 +200,13 @@ function initHistoryHandler() {
     });
 
     // Ashlin BPG Marketing
-    titleList.eq(4).tipso({
+    titleList.eq(5).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
         tooltipHover: true,
         titleContent: "<strong>Ashlin BPG Marketing</strong>",
-        position: "bottom-right",
+        position: "bottom-left",
         width: 300,
         content: "<ul>" +
                 "<li><b>$</b> Independently designed, developed, and tested various financial applications.</li>" +
@@ -194,25 +219,25 @@ function initHistoryHandler() {
     });
 
     // University of Waterloo
-    titleList.eq(5).tipso({
-        background: "white",
-        titleBackground: "#c3c3c3",
-        color: "black",
-        tooltipHover: true,
-        titleContent: "<strong>University of Waterloo</strong>",
-        position: "bottom-left",
-        content: "<p>Current: 4A</p><br><p>1.Compilers</p><p>2.Operating Systems</p><p>3.Computer Networks</p><p>4.Distributed Computing</p>" +
-                 "<p>5.Computer Security</p><br><a style='color:black' target='_blank' href='https://uwaterloo.ca'>See Website</a>"
-    });
-
-    // Shanghai United International School
     titleList.eq(6).tipso({
         background: "white",
         titleBackground: "#c3c3c3",
         color: "black",
         tooltipHover: true,
-        titleContent: "<strong>Shanghai United International School</strong>",
+        titleContent: "<strong>University of Waterloo</strong>",
         position: "bottom-right",
+        content: "<p>Current: 4A</p><br><p>1.Compilers</p><p>2.Operating Systems</p><p>3.Computer Networks</p><p>4.Distributed Computing</p>" +
+                 "<p>5.Computer Security</p><br><a style='color:black' target='_blank' href='https://uwaterloo.ca'>See Website</a>"
+    });
+
+    // Shanghai United International School
+    titleList.eq(7).tipso({
+        background: "white",
+        titleBackground: "#c3c3c3",
+        color: "black",
+        tooltipHover: true,
+        titleContent: "<strong>Shanghai United International School</strong>",
+        position: "bottom-left",
         content: "<p>2nd Place - Class of 2015</p><br><p>AP Cusculus: 94</p><p>Physics: 98</p><p>Chemistry: 95</p>" +
                  "<p>Geography: 93</p><br><a style='color:black' target='_blank' href='http://www.suis.com.cn'>See Website</a>"
     });
